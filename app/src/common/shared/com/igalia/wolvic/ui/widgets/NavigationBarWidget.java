e86380a6f5b693b23ee3cfb150dedb121f913e08
Songlin Jiang <sjiang@igalia.com>
2023-08-04 10:54:37 +0300

    Move Web Apps from Library to Tray
    Signed-off-by: Songlin Jiang <sjiang@igalia.com>

--- app/src/common/shared/com/igalia/wolvic/ui/widgets/NavigationBarWidget.java
+++ app/src/common/shared/com/igalia/wolvic/ui/widgets/NavigationBarWidget.java
@@ -1272,6 +1272,11 @@ public class NavigationBarWidget extends UIWidget implements WSession.Navigation
         onLibraryClicked();
     }
 
+    @Override
+    public void onWebAppsClicked() {
+        onLibraryClicked();
+    }
+
     private void finishWidgetResize() {
         mWidgetManager.finishWidgetResize(mAttachedWindow);
     }
